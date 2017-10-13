a2190b795ed63506d271a141d72bfb4f67ceb667
Aymen Mehri <aymen.mehri@soat.fr>
2017-10-13 12:38:33 +0200

    some fix

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,3 @@
-import { Tab2Page } from './../tab2/tab2';
-import { Tab1Page } from './../tab1/tab1';
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, Nav } from 'ionic-angular';
 
@@ -24,8 +22,8 @@ export class MenuPage {
   @ViewChild(Nav) nav: Nav;
 
   pages: PageInterface[] = [
-    { title: 'Tab 1', pageName: 'TabsPage', tabComponent: Tab1Page, index: 0, icon: 'home' },
-    { title: 'Tab 2', pageName: 'TabsPage', tabComponent: Tab2Page, index: 1, icon: 'contacts' },
+    { title: 'Tab 1', pageName: 'TabsPage', tabComponent: 'Tab1Page', index: 0, icon: 'home' },
+    { title: 'Tab 2', pageName: 'TabsPage', tabComponent: 'Tab2Page', index: 1, icon: 'contacts' },
     { title: 'Special', pageName: 'SpecialPage', icon: 'shuffle' },
   ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
